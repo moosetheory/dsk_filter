3636aa9200e382363a2ddcc9e45c7f8ac760ef48
Annika Merris <annika@adhdgirl.dev>
2026-01-05 12:42:12 +0000

    Adding Puppy Stuff

--- src/components/views/MainView.vue
+++ src/components/views/MainView.vue
@@ -3,6 +3,7 @@ import { Button, Checkbox, Column, DataTable } from 'primevue'
 import { onMounted } from 'vue'
 import { useContaminantStore } from '../../stores/contaminants'
 import { Contaminant, ContaminantSource } from '../../types/contaminants'
+import { Workbook, WorksheetViewFrozen, WorksheetViewSplit } from 'exceljs'
 
 const contaminantStore = useContaminantStore()
 
@@ -55,12 +56,72 @@ const buildCsv = () => {
   link.click()
   URL.revokeObjectURL(link.href)
 }
+
+const buildExcel = () => {
+  const workbook = new Workbook()
+  workbook.creator = "Ramboll"
+  workbook.calcProperties.fullCalcOnLoad = true
+  const contaminantSheet = workbook.addWorksheet('Contaminants', {})
+  contaminantSheet.views = [
+    {
+      state: 'frozen',
+      xSplit: 1,
+    }
+  ]
+  contaminantSheet.columns = [
+    { header: 'Source - Contaminant', key: 'contaminant' },
+    { header: 'Pathway', key: 'pathway' },
+    { header: 'Receptor', key: 'receptor' },
+    { header: 'Likelihood', key: 'likelihood' },
+    { header: 'Consequence', key: 'consequence' },
+    { header: 'Risk', key: 'risk' },
+    { header: 'Comments', key: 'comments' },
+  ]
+
+  const contaminantsArray = getUniqueContaminants()
+    .flatMap((srcKey: string) =>
+      contaminantStore.contaminants.has(srcKey)
+        ?
+        contaminantStore
+          .contaminants
+          .get(srcKey)
+          ?.other
+          .map((otherBits: Contaminant): string[] =>
+            [
+              contaminantStore.contaminants.get(srcKey)?.name ?? "error",
+              otherBits.pathway,
+              otherBits.receptor,
+              "",
+              "",
+              "",
+              "",
+            ]
+          )
+        : []
+    )
+  contaminantSheet.addRows(contaminantsArray)
+  var startCell = 0
+  var previousValue = "";
+  contaminantsArray.forEach((val: string[] | undefined, idx: number) => {
+    if (val === undefined) return;
+    if (val[0] !== previousValue) {
+      if (startCell != 0) {
+        contaminantSheet.mergeCells(`A${startCell}:A${idx}`)
+        console.log(`"A${startCell}:A${idx}"`)
+      }
+      startCell = idx + 1
+      previousValue = val[0]
+    }
+  })
+}
+
 </script>
 
 <template>
   <div>
     <div>
       <Button label="Prepare File" @click="buildCsv()" />
+      <Button label="Do Excel Things" @click="buildExcel()" />
     </div>
     <div v-if="contaminantStore.isLoaded">
       <DataTable :value="contaminantStore.sources">
